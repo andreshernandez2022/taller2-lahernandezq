9514d0c186fa961bd3639ebe25029f12964e8195
andreshernandez2022 <harnandezandres059@gmail.com>
2023-04-13 18:42:28 -0500

    ajuste en el index.html y creacion del evento click del formulario

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('hola mundo');
+const form = document.getElementById('form') as HTMLFormElement;
+const btnGuardar = document.getElementById('btn-guardar') as HTMLButtonElement;
+
+   
+btnGuardar.addEventListener('click', (e) => {
+    //e.preventDefault();
+    console.log("ingreso a la validacion del formulario");
+
+     //OBTENER LOS VALORES DE LOS INPUTS
+     const tipoDocumento = document.getElementById('tipoDocumento') as HTMLInputElement;
+     const numeroDocumento = document.getElementById('numeroDocumento') as HTMLInputElement;
+     const nombre = document.getElementById('nombre') as HTMLInputElement;
+     const apellido = document.getElementById('apellido') as HTMLInputElement;
+     const celular = document.getElementById('celular') as HTMLInputElement;
+     const email = document.getElementById('email') as HTMLInputElement;
+     const likedin = document.getElementById('likedin') as HTMLInputElement;
+     const github = document.getElementById('github') as HTMLInputElement;
+
+     //VALIDACION DEL FORMULARIO
+     console.log(tipoDocumento.value);
+
+});
